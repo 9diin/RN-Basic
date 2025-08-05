72796cc541cebd7645f02af13f3c205357c06704
9Diin <weartstudio@naver.com>
2025-08-05 09:54:23 +0900

    🍀 [init] RN 구글 맵 연동 코드 추가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,6 +17,7 @@ export default function TabsLayout() {
         >
             <Tabs.Screen name="index" options={{ title: "홈", headerShown: false, tabBarIcon: ({ color }) => <Home size={20} color={color} /> }} />
             <Tabs.Screen name="search" options={{ title: "검색", headerShown: false, tabBarIcon: ({ color }) => <Search size={20} color={color} /> }} />
+            <Tabs.Screen name="test" options={{ title: "검색", headerShown: false, tabBarIcon: ({ color }) => <Search size={20} color={color} /> }} />
         </Tabs>
     );
 }
